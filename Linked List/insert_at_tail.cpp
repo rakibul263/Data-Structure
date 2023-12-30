e474aa094cd928f4e0ccdb8fa2927f76baa059eb
Md Rakibul Hasan <shuvo@Mds-MacBook-Air.local>
2023-12-30 20:49:35 +0600

    singly linked list insert at tail

---
+++ Linked List/insert_at_tail.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int value;
+    Node *next;
+    Node(int value)
+    {
+        this->value = value;
+        this->next = NULL;
+    }
+};
+
+void insert_at_tail(Node *&head, int n)
+{
+    Node *newNode = new Node(n);
+    if (head == NULL)
+    {
+        head = newNode;
+    }
+    Node *temp = head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+}
+
+void print_linked_list(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->value<<" ";
+        temp = temp->next;
+    }
+}
+
+int main()
+{
+    Node *head = new Node(10);
+    Node *a = new Node(20);
+    Node *b = new Node(30);
+    
+    //connection
+    head->next = a;
+    a->next = b;
+    
+
+    int tail_node;
+    cin >> tail_node;
+    insert_at_tail(head, tail_node);
+    print_linked_list(head);
+
+    return 0;
+}
