d4c0cd4126e1f313dc79b0aba65c11a0d44b69bb
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2024-01-01 12:24:20 +0600

    delete from head

---
+++ Linked List/delete_head.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int value;
+    Node *next;
+    Node(int value)
+    {
+        this->value = value;
+        this->next = NULL;
+    }
+};
+
+void delete_head(Node *&head)
+{
+    Node *deleteHead = head;
+    head = head->next;
+    delete deleteHead;
+}
+
+void print_linked_list(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->value << " ";
+        temp = temp->next;
+    }
+}
+int main()
+{
+    Node *head = new Node(10);
+    Node *a = new Node(20);
+    Node *b = new Node(30);
+
+    head->next = a;
+    a->next = b;
+
+    delete_head(head);
+    print_linked_list(head);
+
+    return 0;
+}
