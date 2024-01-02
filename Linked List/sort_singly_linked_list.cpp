5ef10fce14442bc19c89e45b3311ae70b30b075f
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2024-01-02 22:48:59 +0600

    singly linked list

---
+++ Linked List/sort_singly_linked_list.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int value;
+    Node *next;
+    Node(int value)
+    {
+        this->value = value;
+        this->next = NULL;
+    }
+};
+
+void insert_at_tail(Node *&head, int val)
+{
+    Node *newNode = new Node(val);
+    if (head == NULL)
+    {
+        head = newNode;
+        return;
+    }
+    Node *temp = head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+}
+
+void print_linked_list(Node *head)
+{
+    Node *temp = head;
+
+    while (temp != NULL)
+    {
+        cout << temp->value << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    Node *head = NULL;
+    int n;
+    while (true)
+    {
+        cin >> n;
+        if (n == -1)
+        {
+            break;
+        }
+        else
+        {
+            insert_at_tail(head, n);
+        }
+    }
+    for (Node *i = head; i->next != NULL; i = i->next)
+    {
+        for (Node *j = i->next; j != NULL; j = j->next)
+        {
+            if (i->value > j->value)
+            {
+                swap(i->value, j->value);
+            }
+        }
+    }
+    print_linked_list(head);
+
+    return 0;
+}
