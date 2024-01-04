0014320f719e74750350d170a050b0bd3c39219b
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2024-01-04 12:57:18 +0600

    doubly linked list delete from position

---
+++ Linked List/delete_node_doubly_ll.cpp
@@ -0,0 +1,122 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int value;
+    Node *next;
+    Node *prev;
+    Node(int value)
+    {
+        this->value = value;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+
+int size_calculate(Node *head)
+{
+    Node *temp = head;
+    int count = 0;
+    while (temp != NULL)
+    {
+        count++;
+        temp = temp->next;
+    }
+    return count;
+}
+
+void delete_from_any_position(Node *head, int pos)
+{
+    Node *temp = head;
+    for (int i = 0; i < pos - 1; i++)
+    {
+        temp = temp->next;
+    }
+    Node *deleteNode = temp->next;
+    temp->next = temp->next->next;
+    temp->next->prev = temp;
+    delete deleteNode;
+}
+
+void delete_from_head(Node *&head, Node *&tail)
+{
+    Node *deleteNode = head;
+    head = head->next;
+    delete deleteNode;
+    if (head == NULL)
+    {
+        tail = NULL;
+        return;
+    }
+    head->prev = NULL;
+}
+
+void delete_from_tail(Node *&head, Node *&tail)
+{
+    Node *deleteNode = tail;
+    tail = tail->prev;
+    delete deleteNode;
+    if (tail == NULL)
+    {
+        head = NULL;
+        return;
+    }
+    tail->next = NULL;
+}
+void print_normal(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->value << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+void print_reverse(Node *tail)
+{
+    Node *temp = tail;
+    while (temp != NULL)
+    {
+        cout << temp->value << " ";
+        temp = temp->prev;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    Node *head = new Node(10);
+    Node *a = new Node(20);
+    Node *b = new Node(30);
+    Node *c = new Node(40);
+    Node *d = new Node(50);
+    Node *tail = d;
+
+    head->next = a;
+    a->prev = head;
+    a->next = b;
+    b->prev = a;
+    b->next = c;
+    c->prev = b;
+    c->next = d;
+    d->prev = c;
+
+    int size = size_calculate(head);
+    print_normal(head);
+    int pos;
+    cout << "Enter a positon for value Delete: ";
+    cin >> pos;
+    if (pos == 0)
+        delete_from_head(head, tail);
+    else if (pos == size)
+        delete_from_tail(head, tail);
+    else
+        delete_from_any_position(head, pos);
+
+    print_normal(head);
+    print_reverse(tail);
+    return 0;
+}
