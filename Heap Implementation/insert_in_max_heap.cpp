0404e757e1b5043e9db39f1b165039b4d12395d3
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2024-01-17 01:32:57 +0600

    max heap

---
+++ Heap Implementation/insert_in_max_heap.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<int> v = {50, 40, 45, 30, 35, 42, 32, 25, 20, 10};
+    int x;
+    cin >> x;
+    v.push_back(x);//logN
+    int current_index = v.size() - 1;
+    while (current_index != 0)
+    {
+        int parent_index = (current_index - 1) / 2;
+        if (v[parent_index] < v[current_index])
+            swap(v[parent_index], v[current_index]);
+        else
+            break;
+        current_index = parent_index;
+    }
+    for(int val : v) cout<<val<<" ";
+    return 0;
+}
