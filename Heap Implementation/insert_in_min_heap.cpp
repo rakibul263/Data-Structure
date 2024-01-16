7acbd61c07ee0ff9251185d05effaeaa53d3da46
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2024-01-17 01:47:15 +0600

    complexity logN

---
+++ Heap Implementation/insert_in_min_heap.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<int> v;
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        v.push_back(x); // logN
+        int current_index = v.size() - 1;
+        while (current_index != 0)
+        {
+            int parent_index = (current_index - 1) / 2;
+            if (v[parent_index] > v[current_index])
+                swap(v[parent_index], v[current_index]);
+            else
+                break;
+            current_index = parent_index;
+        }
+    }
+    for (int val : v)
+        cout << val << " ";
+    return 0;
+}
