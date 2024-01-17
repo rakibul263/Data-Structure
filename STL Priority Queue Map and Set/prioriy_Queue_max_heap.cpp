454ae8d57067d815ec621ef280935767bbd9197a
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2024-01-17 21:01:36 +0600

    create priority queue with max heap

---
+++ STL Priority Queue Map and Set/prioriy_Queue_max_heap.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    priority_queue<int> pq;
+    while (true)
+    {
+        int x;
+        cin >> x;
+        if (x == 0)
+        {
+            int p;
+            cin >> p;
+            pq.push(p);//logN
+        }
+        else if (x == 1)
+        {
+            pq.pop();//logN
+        }
+        else if (x == 2)
+        {
+            cout << pq.top() << endl;//O(1)
+        }
+        else
+        {
+            break;
+        }
+    }
+    return 0;
+}
